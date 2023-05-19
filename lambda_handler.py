0346655463e3dba3120b50b1a68d754da4a3a926
turner <turnermluke@gmail.com>
2023-05-18 20:25:47 -0500

    Testing.

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,15 +1,12 @@
-from src.aws_helpers.s3 import read_from_s3
+import config
 
+from src.aws_helpers.s3 import read_from_s3
+from src.slackbot.messages import send_message_to_slack_channel
 
 def handler(event, context):
-    print("Version 2:")
-    msg = event['msg']
-    print("Message: " + msg)
-
-    # read the file
-    text = read_from_s3('ziki-analytics-config', 'test.txt')
-    print(text)
+    send_message_to_slack_channel("TEST")
 
 
 if __name__ == '__main__':
+
     handler({'msg': 'Hello World!'}, None)
