52fc1aaa5f30643e89cd534746971cf0a5953406
turner <turnermluke@gmail.com>
2023-05-26 14:45:15 -0500

    Added `save_df_as_csv`.

--- src/aws_helpers/s3.py
+++ src/aws_helpers/s3.py
@@ -1,6 +1,9 @@
+import os
 import gzip
 import json
+import io
 
+import pandas as pd
 import boto3
 
 s3 = boto3.client('s3')
@@ -36,3 +39,39 @@ def write_to_s3(bucket_name, file_name, data):
         data = json.dumps(data)
     # upload the file
     s3.put_object(Body=data, Bucket=bucket_name, Key=file_name)
+
+
+def save_df_as_csv(df, bucket_name, csv_path):
+    """
+    Save a Pandas DataFrame as a CSV file in S3.
+    :param df: Pandas DataFrame
+    :param bucket_name: S3 bucket name
+    :param csv_path: S3 path to the CSV file
+    :return:
+    """
+    # Save the DataFrame as a local CSV file
+    local_csv_path = 'temp.csv'
+    df.to_csv(local_csv_path, index=False)
+
+    # Upload the local CSV file to S3
+    s3.upload_file(local_csv_path, bucket_name, csv_path)
+
+    os.remove(local_csv_path)
+
+
+def get_csv_as_df(bucket_name, csv_path):
+    """
+    Read a CSV file from S3 and return a Pandas DataFrame.
+    :param bucket_name: S3 bucket name
+    :param csv_path: S3 path to the CSV file
+    :return: Pandas DataFrame
+    """
+    obj = s3.get_object(Bucket=bucket_name, Key=csv_path)
+    df = pd.read_csv(io.BytesIO(obj['Body'].read()))
+
+    return df
+
+
+if __name__ == '__main__':
+    df = get_csv_as_df('ziki-analytics-cleaned-datasets', 'sales.csv')
+    print(df)
