a13d3f9124a5d9f324bc74eeded8212eb1c1f6c4
turner <turnermluke@gmail.com>
2023-05-29 13:20:12 -0500

    Updated to new deployment.

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 URL_STRING=".dkr.ecr.us-east-1.amazonaws.com"
 CONTAINER_STRING=$(basename "$(pwd)")
 IMAGE_STRING="latest"
@@ -13,10 +15,25 @@ docker rmi "$AWS_ACCOUNT_ID$URL_STRING/$CONTAINER_STRING" "$CONTAINER_STRING"
 # Build image
 docker build --tag "$CONTAINER_STRING" .
 
+# Run the Docker image locally
+docker run -d --name "$CONTAINER_STRING" "$CONTAINER_STRING"
+
+## Wait for the container to start
+#sleep 5  # TODO: See if this can be removed
+
+# Execute tests against the running container
+# Replace the following command with your test command
+docker exec "$CONTAINER_STRING" python -m pytest
+
+# Stop and remove the local container
+docker stop "$CONTAINER_STRING"
+docker rm "$CONTAINER_STRING"
+
 # Check if ECR repository exists, create if it doesn't
 ECR_REPOSITORY_NAME="$CONTAINER_STRING"
-ECR_REPOSITORY_EXISTS=$(aws ecr describe-repositories --repository-names "$ECR_REPOSITORY_NAME" --query 'repositories' --output text)
+ECR_REPOSITORY_EXISTS=$(aws ecr describe-repositories --repository-names "$ECR_REPOSITORY_NAME" --query 'repositories' --output text 2>/dev/null)
 if [ -z "$ECR_REPOSITORY_EXISTS" ]; then
+    echo "ECR repository $ECR_REPOSITORY_NAME does not exist"
     echo "Creating ECR repository $ECR_REPOSITORY_NAME"
     aws ecr create-repository --repository-name "$ECR_REPOSITORY_NAME"
 fi
@@ -29,6 +46,7 @@ docker push "$ECR_IMAGE_URI"
 LAMBDA_FUNCTION_NAME="$CONTAINER_STRING"
 LAMBDA_FUNCTION_EXISTS=$(aws lambda list-functions --query "Functions[?FunctionName=='$LAMBDA_FUNCTION_NAME'].FunctionName" --output text)
 if [ -z "$LAMBDA_FUNCTION_EXISTS" ]; then
+    echo "Lambda function $LAMBDA_FUNCTION_NAME does not exist"
     echo "Creating Lambda function $LAMBDA_FUNCTION_NAME"
     aws lambda create-function --function-name "$LAMBDA_FUNCTION_NAME" --code "ImageUri=$ECR_IMAGE_URI" --role "$LAMBDA_EXECUTION_ROLE_ARN" --package-type Image
 else
