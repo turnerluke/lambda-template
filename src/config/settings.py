1a2af7154c1f03091841b59a8ac0095859384a6d
turner <turnermluke@gmail.com>
2023-05-31 14:56:27 -0500

    Handling different structure in AWS.

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,9 +3,13 @@ import yaml
 
 
 def unpack_yaml():
-    # project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # For local
-    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # For lambda
-    yaml_path = os.path.join(project_root, 'config.yaml')
+    project_root_local = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # For local
+    project_root_aws = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # For lambda
+
+    yaml_local = os.path.join(project_root_local, 'config.yaml')
+    yaml_aws = os.path.join(project_root_aws, 'config.yaml')
+    yaml_path = yaml_local if os.path.exists(yaml_local) else yaml_aws
+    #yaml_path = os.path.join(project_root, 'config.yaml')
     with open(yaml_path, 'r') as f:
         config = yaml.safe_load(f)
     for k, v in config.items():
